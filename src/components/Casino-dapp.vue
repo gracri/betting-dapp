d391960e41352845431fbbdd4aa6ccfbb7316139
Christine Graff <chrisgraff@protonmail.com>
2018-10-05 18:47:29 +0200

    connect to contract, betting function

--- src/components/Casino-dapp.vue
+++ src/components/Casino-dapp.vue
@@ -6,12 +6,50 @@
 import HelloMetamask from '@/components/Hello-metamask'
 export default {
   name: 'casino-dapp',
+  data () {
+    return {
+      amount: null,
+      pending: false,
+      winEvent: null
+    }
+  },
+  methods: {
+    clickNumber (event) {
+      console.log(event.target.innerHTML, this.amount)
+      this.winEvent = null
+      this.pending = true
+      this.$store.state.contractInstance().bet(event.target.innerHTML, {
+        gas: 300000,
+        value: this.$store.state.web3.web3Instance().toWei(this.amount, 'ether'),
+        from: this.$store.state.web3.coinbase
+      }, (err, result) => {
+        if (err) {
+          console.log(err)
+          this.pending = false
+        } else {
+          let Won = this.$store.state.contractInstance().Won()
+          Won.watch((err, result) => {
+            if (err) {
+              console.log('could not get event Won()')
+            } else {
+              this.winEvent = result.args
+              this.pending = false
+            }
+          })
+        }
+      })
+    }
+  },
   beforeCreate () {
     console.log('registerWeb3 Action dispatched from casino-dapp.vue')
     this.$store.dispatch('registerWeb3')
   },
   components: {
     'hello-metamask': HelloMetamask
+  },
+  mounted () {
+    console.log('dispatching getContractInstance')
+    this.$store.dispatch('getContractInstance')
   }
 }
 

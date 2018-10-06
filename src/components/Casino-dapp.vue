80eb7bba7af6a3952ed6b25d737057f23971665b
Christine Graff <chrisgraff@protonmail.com>
2018-10-07 00:56:31 +0200

    display metamask stats and spinner

--- src/components/Casino-dapp.vue
+++ src/components/Casino-dapp.vue
@@ -1,4 +1,6 @@
 <template>
+  <div class="">
+  <hello-metamask />
  <div class="casino">
    <h1>Welcome to the Casino</h1>
    <h4>Please pick a number between 1 and 10</h4>
@@ -15,20 +17,29 @@
      <li v-on:click="clickNumber">9</li>
      <li v-on:click="clickNumber">10</li>
   </ul>
-  <img v-if="pending" id="loader" src=”https://loading.io/spinners/double-ring/lg.double-ring-spinner.gif”>
+  <img v-if="pending" id="loader" src="https://loading.io/spinners/double-ring/lg.double-ring-spinner.gif">
   <div class="event" v-if="winEvent">
-    Won: {{ winEvent._status }}
-    Amount: {{ winEvent._amount }} Wei
+    <p v-if="winEvent._status" id="has-won"><i aria-hidden="true" class="fa fa-check"></i> Congragulations, you have won {{winEvent._amount}} wei</p>
+    <p v-else id="has-lost"><i aria-hidden="true" class="fa fa-times"></i> Sorry you lost, try again.</p>
   </div>
  </div>
+</div>
 </template>
 
 
 
 <script>
 import HelloMetamask from '@/components/Hello-metamask'
+
 export default {
   name: 'casino-dapp',
+  beforeCreate () {
+    console.log('registerWeb3 Action dispatched from casino-dapp.vue')
+    this.$store.dispatch('registerWeb3')
+  },
+  components: {
+    'hello-metamask': HelloMetamask
+  },
   data () {
     return {
       amount: null,
@@ -37,82 +48,82 @@ export default {
     }
   },
   methods: {
-    clickNumber (event) {
-      console.log(event.target.innerHTML, this.amount)
-      this.winEvent = null
-      this.pending = true
-      this.$store.state.contractInstance().bet(event.target.innerHTML, {
-        gas: 300000,
-        value: this.$store.state.web3.web3Instance().toWei(this.amount, 'ether'),
-        from: this.$store.state.web3.coinbase
-      }, (err, result) => {
-        if (err) {
-          console.log(err)
-          this.pending = false
-        } else {
-          let Won = this.$store.state.contractInstance().Won()
-          Won.watch((err, result) => {
-            if (err) {
-              console.log('could not get event Won()')
-            } else {
-              this.winEvent = result.args
-              this.pending = false
-            }
-          })
-        }
-      })
-    }
-  },
-  beforeCreate () {
-    console.log('registerWeb3 Action dispatched from casino-dapp.vue')
-    this.$store.dispatch('registerWeb3')
-  },
-  components: {
-    'hello-metamask': HelloMetamask
-  },
-  mounted () {
-    console.log('dispatching getContractInstance')
-    this.$store.dispatch('getContractInstance')
+  clickNumber (event) {
+    console.log('BETTING ON NUMBER, AMOUNT', event.target.innerHTML, this.amount)
+    this.winEvent = null
+    this.pending = true
+    this.$store.state.contractInstance().bet(event.target.innerHTML, {
+      gas: 300000,
+      value: this.$store.state.web3.web3Instance().toWei(this.amount, 'ether'),
+      from: this.$store.state.web3.coinbase
+    }, (err, result) => {
+      if (err) {
+        console.log(err)
+        this.pending = false
+      } else {
+        let Won = this.$store.state.contractInstance().Won()
+        Won.watch((err, result) => {
+          if (err) {
+            console.log('could not get event Won()')
+          } else {
+            this.winEvent = result.args
+            this.winEvent._amount = parseInt(result.args._amount, 10)
+            this.pending = false
+          }
+        })
+      }
+    })
   }
+},
+mounted () {
+  console.log('dispatching getContractInstance')
+  this.$store.dispatch('getContractInstance')
+}
 }
 
 </script>
 
 <style scoped>
 .casino {
- margin-top: 50px;
- text-align:center;
+     margin-top: 50px;
+     text-align:center;
 }
 #loader {
- width:150px;
+  width:150px;
 }
 ul {
- margin: 25px;
- list-style-type: none;
- display: grid;
- grid-template-columns: repeat(5, 1fr);
- grid-column-gap:25px;
- grid-row-gap:25px;
+    margin: 25px;
+    list-style-type: none;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-column-gap:25px;
+    grid-row-gap:25px;
 }
 li{
- padding: 20px;
- margin-right: 5px;
- border-radius: 50%;
- cursor: pointer;
- background-color:#fff;
- border: -2px solid #bf0d9b;
- color: #bf0d9b;
- box-shadow:3px 5px #bf0d9b;
+    padding: 20px;
+    margin-right: 5px;
+    border-radius: 50%;
+    cursor: pointer;
+    background-color:#fff;
+    border: -2px solid #bf0d9b;
+    color: #bf0d9b;
+    box-shadow:3px 5px #bf0d9b;
 }
 li:hover{
- background-color:#bf0d9b;
- color:white;
- box-shadow:0px 0px #bf0d9b;
+    background-color:#bf0d9b;
+    color:white;
+    box-shadow:0px 0px #bf0d9b;
 }
 li:active{
- opacity: 0.7;
+    opacity: 0.7;
 }
 *{
- color: #444444;
+   color: #444444;
+}
+#has-won {
+  color: green;
+}
+#has-lost {
+  color:red;
 }
 </style>
